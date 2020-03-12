b32d28c5fb7fe8ec126ba14db1b65d918b0bb4a9
Jorge Andrada <jandrada@viewnext.com>
2020-03-12 20:22:22 +0100

    fix var

--- assets/test/test.sh
+++ assets/test/test.sh
@@ -15,7 +15,11 @@ freeMem=`awk '/MemFree/ { print int($2/1024) }' /proc/meminfo`
 s=$(($freeMem/10*8))
 x=$(($freeMem/10*8))
 n=$(($freeMem/10*2))
-export JVM_ARGS="-Xmn${n}m -Xms${s}m -Xmx${x}m"
+
+if [ -z "$JVM_ARGS" ]; then {
+  export JVM_ARGS="-Xmn${n}m -Xms${s}m -Xmx${x}m"
+}
+fi
 
 echo "START Running Jmeter on `date`"
 echo "JVM_ARGS=${JVM_ARGS}"
